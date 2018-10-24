bfba9e45158b165e71a943bb3bd2f1458b5a77a5
abhishek <abhishek@om12.om.omnieserver.com>
2018-10-24 14:58:10 +0530

    First Commit

---
+++ NewTestProject/src/sum/SumAdd.java
@@ -0,0 +1,13 @@
+package sum;
+
+public class SumAdd {
+
+	public static void main(String[] args) {
+
+		int a=10 ,b=20;
+		int sum = a+b;
+		System.out.println("These are the number: "+sum);
+		
+	}
+
+}
