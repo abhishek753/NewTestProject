085ae375acc342365e5aec9564f3037627342b13
abhishek <abhishek@om12.om.omnieserver.com>
2018-11-20 19:39:35 +0530

    Sixth Commit

--- NewTestProject/src/loops/WhileLoop.java
+++ NewTestProject/src/loops/WhileLoop.java
@@ -2,6 +2,14 @@ package loops;
 
 public class WhileLoop {
 
+	/*
+	 * 
+	 * Loops are not Statements ---> they are not terminated but exits.
+	 * Entry criteria ---> Loop condition must be True.
+	 * Exits criteria ---> Loop condition is False.
+	 * 
+	 */
+	
 	public static void main(String[] args) {
 		int i=1;
 		while(i<=10) {
