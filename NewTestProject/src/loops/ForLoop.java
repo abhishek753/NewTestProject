eaf2cb2d2df6efc8ba7690876b5665bbe096c723
abhishek <abhishek@om12.om.omnieserver.com>
2018-11-20 20:18:02 +0530

    Seventh Commit

---
+++ NewTestProject/src/loops/ForLoop.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class ForLoop {
+/*
+ * While Loop ---> When the number of iterations are unkonwn.
+ * For Loop ---> Number of iterations are known.
+ * Do While Loop ---> at least for one time irrespective of the condition.
+ * 
+ * 
+ */
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
