fb9a1b0793fd1332f5889fadd34ad1262b0fccd5
abhishek <abhishek@om12.om.omnieserver.com>
2018-12-10 19:56:06 +0530

    Fiften commit

---
+++ NewTestProject/src/set/TreeSet_Test.java
@@ -0,0 +1,27 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSet_Test {
+
+	public static void main(String[] args) {
+		
+//		Set<String> ts = new se
+//		ts.add("A");
+//		ts.add("B");
+//		ts.add("C");
+//		System.out.println(ts);
+//		ts.add("C");
+//		System.out.println(ts);
+				
+		Set<String> set= new HashSet<String>();
+		set.add("A");
+		set.add("A");
+		System.out.println(set);
+		
+
+	}
+
+}
